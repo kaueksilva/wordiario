de5b1a7afb7aa0d172a827a9f8cb5d6b4f0eeea4
Kauê Silva <kaueksilva12@gmail.com>
2024-09-27 13:08:14 -0300

    menu e home

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,28 @@
 'use client';
 import Image from 'next/image';
+import { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
+import { FaMinus } from 'react-icons/fa';
+import { IoIosArrowDown } from 'react-icons/io';
 
 const Menu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [dropdowns, setDropdowns] = useState({
+    Registros: false,
+    Publicacoes: false,
+  });
+
+  const toggleDropdown = (menu) => {
+    setDropdowns((prev) => ({
+      ...prev,
+      [menu]: !prev[menu],
+    }));
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <div>
       <header>
@@ -21,12 +42,7 @@ const Menu = () => {
             <ul className="menu-topo hidden lg:flex gap-3 mr-11 font-open-sans">
               <li className="nav-topo">
                 <a href="#" className="nav-link text-white text-[12px] hover:underline">
-                  Diário Oficial
-                </a>
-              </li>
-              <li className="nav-topo">
-                <a href="#" className="nav-link text-white text-[12px] hover:underline">
-                  Portal da Transparência
+                  Site Oficial
                 </a>
               </li>
               <li className="nav-topo">
@@ -36,22 +52,12 @@ const Menu = () => {
               </li>
               <li className="nav-topo">
                 <a href="#" className="nav-link text-white text-[12px] hover:underline">
+                  Portal da Transparência
+                </a>
+              </li>
+              <li className="nav-topo">
+                <a href="#" className="nav-link text-white text-[12px] hover:underline">
                   Portal do Contribuinte
-                </a>
-              </li>
-              <li className="nav-topo">
-                <a href="#" className="nav-link text-white text-[12px] hover:underline">
-                  Portal do Servidor
-                </a>
-              </li>
-              <li className="nav-topo">
-                <a href="#" className="nav-link text-white text-[12px] hover:underline">
-                  COVID-19
-                </a>
-              </li>
-              <li className="nav-topo">
-                <a href="#" className="nav-link text-white text-[12px] hover:underline">
-                  Radar da Transparência
                 </a>
               </li>
               <li>
@@ -61,6 +67,204 @@ const Menu = () => {
               </li>
             </ul>
           </div>
+
+          {/* Menu Principal */}
+          <div className="menus">
+            <div className="navbar fixed top-[38px] w-full z-40 bg-[#fffffff1] flex justify-between items-center p-[13px] pl-[20px] pr-[10px]">
+              <a onClick={() => (window.location.href = '/')} href="#" className="logo pl-7">
+                <Image
+                  src="/images/logo1.png"
+                  alt="Logo do Menu"
+                  width={749}
+                  height={190}
+                  className="w-[210px] h-auto"
+                />
+              </a>
+
+              <div className="lg:hidden pr-6">
+                <button onClick={toggleMenu} className="text-[#224276]">
+                  <i className="ri-menu-3-line text-[24px]"></i>
+                </button>
+              </div>
+
+              <ul className="nav-menu hidden lg:flex gap-8 pr-11 text-[#000000] font-open-sans text-[14px]">
+                {/* Atas de Registro de Preços */}
+                <li
+                  className="nav-item relative"
+                  onMouseEnter={() => toggleDropdown('Registros')}
+                  onMouseLeave={() => toggleDropdown('Registros')}
+                >
+                  <a href="#" className="text-[#333333] hover:text-[#7baeff] flex items-center py-2">
+                    <span>Atas de Registro de Preços</span>
+                    <IoIosArrowDown />
+                  </a>
+
+                  {dropdowns.Registros && (
+                    <ul className="absolute border-t-[3px] border-[#7baeff] w-[185px] bg-white shadow-lg">
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2024
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2023
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2022
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2021
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2020
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2019
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2018
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2017
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2016
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Registro de Preço - 2015
+                        </a>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+
+                {/* Publicações Anteriores do DO */}
+                <li
+                  className="nav-item relative"
+                  onMouseEnter={() => toggleDropdown('Publicacoes')}
+                  onMouseLeave={() => toggleDropdown('Publicacoes')}
+                >
+                  <a href="#" className="text-[#333333] hover:text-[#7baeff] flex items-center py-2">
+                    <span>Publicações Anteriores do DO</span>
+                    <IoIosArrowDown />
+                  </a>
+
+                  {dropdowns.Publicacoes && (
+                    <ul className="absolute border-t-[3px] border-[#7baeff] w-[185px] bg-white shadow-lg">
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2024
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2023
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2022
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2021
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2020
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2019
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2018
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2017
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2016
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" className="block py-2 text-[#000000] text-[14px] border-b-[1px] border-[#c4c4c4]">
+                          Diário Oficial - 2015
+                        </a>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Menu Responsivo */}
+          {isMenuOpen && (
+            <div className="fixed inset-0 z-50 bg-[#003470dc] text-white flex flex-col items-center justify-center p-8 lg:hidden">
+              {''}
+              {/* Adiciona lg:hidden */}
+              <div className="icones flex gap-5 mb-10">
+                <i className="ri-twitter-x-line text-white text-[24px]"></i>
+                <i className="ri-facebook-circle-fill text-white text-[24px]"></i>
+                <i className="ri-instagram-line text-white text-[24px]"></i>
+                <i className="ri-youtube-fill text-white text-[24px]"></i>
+              </div>
+              <ul className="text-center text-[20px] font-bold">
+                <li className="mb-6">
+                  <a
+                    href="#"
+                    onClick={() => toggleDropdown('Registros')}
+                    className={`flex items-center justify-center 
+                      ${dropdowns.Registros ? 'border-[1px] border-[#ffffff7c]' : ''}`}
+                  >
+                    Atas de Registro de Preços
+                    {dropdowns.Registros ? (
+                      <FaMinus className="ml-2 text-[14px] text-white" />
+                    ) : (
+                      <FaPlus className="ml-2 text-[14px] text-white" />
+                    )}
+                  </a>
+                  {dropdowns.Registros && (
+                    <ul className="mt-2 mb-6 text-[14px] leading-3">
+                      <li className="mb-3">
+                        <a href="../mapa_da_estrategia">MAPA DA ESTRATÉGIA</a>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+              </ul>
+              <button onClick={toggleMenu} className="mt-10 text-[24px]">
+                <i className="ri-close-line"></i>
+              </button>
+            </div>
+          )}
         </nav>
       </header>
     </div>
--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,56 +1,36 @@
 import Link from 'next/link';
-
 import { postPathBySlug, sanitizeExcerpt } from 'lib/posts';
-
 import Metadata from 'components/Metadata';
-
 import { FaMapPin } from 'react-icons/fa';
 import styles from './PostCard.module.scss';
 
+// Função para formatar a data
+const formatDate = (dateString) =>
+  new Date(dateString).toLocaleDateString('pt-BR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
 const PostCard = ({ post, options = {} }) => {
-  const { title, excerpt, slug, date, author, categories, isSticky = false } = post;
-  const { excludeMetadata = [] } = options;
-
-  const metadata = {};
-
-  if (!excludeMetadata.includes('author')) {
-    metadata.author = author;
-  }
-
-  if (!excludeMetadata.includes('date')) {
-    metadata.date = date;
-  }
-
-  if (!excludeMetadata.includes('categories')) {
-    metadata.categories = categories;
-  }
-
-  let postCardStyle = styles.postCard;
-
-  if (isSticky) {
-    postCardStyle = `${postCardStyle} ${styles.postCardSticky}`;
-  }
+  const { title, excerpt, slug, isSticky = false, date } = post;
+  const { excludedate = [] } = options;
+  const postCardStyle = isSticky
+    ? `${styles.postCard} 
+    ${styles.postCardSticky}`
+    : styles.postCard;
 
   return (
     <div className={postCardStyle}>
       {isSticky && <FaMapPin aria-label="Sticky Post" />}
       <Link href={postPathBySlug(slug)}>
-        <h3
-          className={styles.postCardTitle}
-          dangerouslySetInnerHTML={{
-            __html: title,
-          }}
-        />
+        <h3 className={styles.postCardTitle} dangerouslySetInnerHTML={{ __html: title }} />
       </Link>
-      <Metadata className={styles.postCardMetadata} {...metadata} />
+      <Metadata className={styles.postCardMetadata} />
       {excerpt && (
-        <div
-          className={styles.postCardContent}
-          dangerouslySetInnerHTML={{
-            __html: sanitizeExcerpt(excerpt),
-          }}
-        />
+        <div className={styles.postCardContent} dangerouslySetInnerHTML={{ __html: sanitizeExcerpt(excerpt) }} />
       )}
+      {!excludedate.includes('date') && <div className={styles.postCardate}> {formatDate(date)} </div>}
     </div>
   );
 };
