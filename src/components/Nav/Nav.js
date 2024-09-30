93c363ed9e325f9e4171304207c47f995d58803d
Kauê Silva <kaueksilva12@gmail.com>
2024-09-30 15:08:45 -0300

    lupa add

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,24 @@
 'use client';
 import Image from 'next/image';
-import { useState } from 'react';
+import styles from './Nav.module.scss';
 import { FaPlus } from 'react-icons/fa';
 import { FaMinus } from 'react-icons/fa';
 import { IoIosArrowDown } from 'react-icons/io';
+import { FaSearch } from 'react-icons/fa';
+
+import { useEffect, useRef, useState, useCallback } from 'react';
+import Link from 'next/link';
+
+// import useSite from 'hooks/use-site';
+import useSearch, { SEARCH_STATE_LOADED } from 'hooks/use-search';
+import { postPathBySlug } from 'lib/posts';
+// import { findMenuByLocation, MENU_LOCATION_NAVIGATION_DEFAULT } from 'lib/menus';
+
+// import Section from 'components/Section';
+// import NavListItem from 'components/NavListItem';
+
+const SEARCH_VISIBLE = 'visible';
+const SEARCH_HIDDEN = 'hidden';
 
 const Menu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,6 +37,124 @@ const Menu = () => {
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
   };
+
+  // {/*Search*/}
+  const formRef = useRef();
+
+  const [searchVisibility, setSearchVisibility] = useState(SEARCH_HIDDEN);
+
+  // const { metadata = {}, menus } = useSite();
+  // const { title } = metadata;
+
+  // const navigationLocation = process.env.WORDPRESS_MENU_LOCATION_NAVIGATION || MENU_LOCATION_NAVIGATION_DEFAULT;
+  // const navigation = findMenuByLocation(menus, navigationLocation);
+
+  const { query, results, search, clearSearch, state } = useSearch({
+    maxResults: 5,
+  });
+
+  const searchIsLoaded = state === SEARCH_STATE_LOADED;
+
+  // When the search visibility changes, we want to add an event listener that allows us to
+  // detect when someone clicks outside of the search box, allowing us to close the results
+  // when focus is drawn away from search
+
+  useEffect(() => {
+    // If we don't have a query, don't need to bother adding an event listener
+    // but run the cleanup in case the previous state instance exists
+
+    if (searchVisibility === SEARCH_HIDDEN) {
+      removeDocumentOnClick();
+      return;
+    }
+
+    addDocumentOnClick();
+    addResultsRoving();
+
+    const searchInput = Array.from(formRef.current.elements).find((input) => input.type === 'search');
+
+    searchInput.focus();
+
+    return () => {
+      removeResultsRoving();
+      removeDocumentOnClick();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchVisibility]);
+
+  function addDocumentOnClick() {
+    document.body.addEventListener('click', handleOnDocumentClick, true);
+  }
+
+  function removeDocumentOnClick() {
+    document.body.removeEventListener('click', handleOnDocumentClick, true);
+  }
+
+  function handleOnDocumentClick(e) {
+    if (!e.composedPath().includes(formRef.current)) {
+      setSearchVisibility(SEARCH_HIDDEN);
+      clearSearch();
+    }
+  }
+
+  function handleOnSearch({ currentTarget }) {
+    search({
+      query: currentTarget.value,
+    });
+  }
+
+  function handleOnToggleSearch() {
+    setSearchVisibility(SEARCH_VISIBLE);
+  }
+
+  function addResultsRoving() {
+    document.body.addEventListener('keydown', handleResultsRoving);
+  }
+
+  function removeResultsRoving() {
+    document.body.removeEventListener('keydown', handleResultsRoving);
+  }
+
+  function handleResultsRoving(e) {
+    const focusElement = document.activeElement;
+
+    if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      if (focusElement.nodeName === 'INPUT' && focusElement.nextSibling.children[0].nodeName !== 'P') {
+        focusElement.nextSibling.children[0].firstChild.firstChild.focus();
+      } else if (focusElement.parentElement.nextSibling) {
+        focusElement.parentElement.nextSibling.firstChild.focus();
+      } else {
+        focusElement.parentElement.parentElement.firstChild.firstChild.focus();
+      }
+    }
+
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      if (focusElement.nodeName === 'A' && focusElement.parentElement.previousSibling) {
+        focusElement.parentElement.previousSibling.firstChild.focus();
+      } else {
+        focusElement.parentElement.parentElement.lastChild.firstChild.focus();
+      }
+    }
+  }
+
+  const escFunction = useCallback((event) => {
+    if (event.keyCode === 27) {
+      clearSearch();
+      setSearchVisibility(SEARCH_HIDDEN);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keydown', escFunction, false);
+
+    return () => {
+      document.removeEventListener('keydown', escFunction, false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
@@ -221,6 +354,49 @@ const Menu = () => {
                     </ul>
                   )}
                 </li>
+
+                {/* Busca */}
+                <div className={styles.navSearch}>
+                  {searchVisibility === SEARCH_HIDDEN && (
+                    <button onClick={handleOnToggleSearch} disabled={!searchIsLoaded}>
+                      <span className="sr-only">Toggle Search</span>
+                      <FaSearch />
+                    </button>
+                  )}
+                  {searchVisibility === SEARCH_VISIBLE && (
+                    <form ref={formRef} action="/search" data-search-is-active={!!query}>
+                      <input
+                        type="search"
+                        name="q"
+                        value={query || ''}
+                        onChange={handleOnSearch}
+                        autoComplete="off"
+                        placeholder="Search..."
+                        required
+                      />
+                      <div className={styles.navSearchResults}>
+                        {results.length > 0 && (
+                          <ul>
+                            {results.map(({ slug, title }, index) => {
+                              return (
+                                <li key={slug}>
+                                  <Link tabIndex={index} href={postPathBySlug(slug)}>
+                                    {title}
+                                  </Link>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        )}
+                        {results.length === 0 && (
+                          <p>
+                            Sorry, not finding anything for <strong>{query}</strong>
+                          </p>
+                        )}
+                      </div>
+                    </form>
+                  )}
+                </div>
               </ul>
             </div>
           </div>
