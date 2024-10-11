d16b15b4359ba53cbae064332b41fcff32d3bfa0
Larissa Machado <larissadsmachado0@gmail.com>
2024-10-11 10:03:43 -0300

    lali

--- src/templates/archive.js
+++ src/templates/archive.js
@@ -57,7 +57,7 @@ export default function TemplateArchive({
 
       <Section>
         <Container>
-          <SectionTitle>Posts</SectionTitle>
+          <SectionTitle></SectionTitle>
           {Array.isArray(posts) && (
             <>
               <ul className={styles.posts}>
